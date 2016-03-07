5e67fbc4c02dc134935230e53a8d376d5e0224d8
Hojin Ghim <beila@kaist.ac.kr>
2016-03-07 19:00:01 +0000

    Change from dev-dsk-hojin-1a-corp-i-51df2cdb.eu-west-1.amazon.com

--- packages/install.sh
+++ packages/install.sh
@@ -15,5 +15,6 @@ for f in $HOME/.linuxbrew/bin; do
     fi
 done
 
-$BREW install perl cmake
+# gcc is needed because of https://github.com/Linuxbrew/linuxbrew/issues/732#issuecomment-192697040
+$BREW install gcc cmake
 
