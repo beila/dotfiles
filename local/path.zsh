2d9642b00d6cb4bceab28991bb320b9fc9acbece
Hojin Ghim <beila@kaist.ac.kr>
2016-03-10 15:54:01 +0000

    Change from lhr15-5453lkq-u

--- local/path.zsh
+++ local/path.zsh
@@ -1,9 +1,9 @@
 LOCAL_ROOT=$HOME/local
 
 if [[ -d $LOCAL_ROOT ]]
-do
+then
   for src in $(find -H "$LOCAL_ROOT" -maxdepth 2 -name 'bin' -type d)
   do
     export PATH=$PATH:$src
   done
-done
+fi
