ed6113382d8d87455d43b84982e268278eaa0eb7
Hojin Ghim <beila@kaist.ac.kr>
2016-03-15 18:29:01 +0000

    Change from acbc32b014df.ant.amazon.com

---
+++ packages/completion.zsh
@@ -0,0 +1,3 @@
+# brew install awscli says:
+# Add the following to ~/.zshrc to enable zsh completion:
+source /usr/local/share/zsh/site-functions/_aws
--- packages/install.sh
+++ packages/install.sh
@@ -15,8 +15,10 @@ for f in $HOME/.linuxbrew/bin; do
     fi
 done
 
-$BREW tap homebrew/versions
+#$BREW tap homebrew/versions
 
 # gcc is needed because of https://github.com/Linuxbrew/linuxbrew/issues/732#issuecomment-192697040
-$BREW install gcc47 cmake
+#$BREW install gcc47 cmake
+
+$BREW install awscli
 
