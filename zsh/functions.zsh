0ebb2dbbeea6f921b14e0477ca134b7397d7fbd2
Hojin Ghim <beila@kaist.ac.kr>
2016-04-22 16:49:00 +0100

    feat(zsh): add crashrate function and auto-load all zsh functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,11 +2,15 @@ local funcdir=$(dirname $(readlink -f $0))/functions
 
 fpath=($funcdir $fpath)
 
+for ff in $(cd $funcdir;ls *)
+do
+	autoload ${ff}
+done
+
 # refer to smart_sudo in http://zshwiki.org/home/examples/functions
 #alias fa='_fa '
 for ff in $(cd $funcdir;ls _*)
 do
-	autoload ${ff}
 	alias ${ff#_}="${ff} "
 done
 
