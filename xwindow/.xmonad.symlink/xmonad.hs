bf8d0c5a791151669742fd55cdc45a903ef9aa76
Hojin Ghim <beila@kaist.ac.kr>
2016-06-20 16:35:01 +0100

    Change from lhr15-5453lkq-u

--- xwindow/.xmonad.symlink/xmonad.hs
+++
@@ -1,8 +0,0 @@
-import XMonad
-import XMonad.Hooks.SetWMName
-
-main = xmonad defaultConfig
-	-- https://bbs.archlinux.org/viewtopic.php?pid=744577#p744577
-	{ startupHook = setWMName "LG3D"
-	, modMask = mod4Mask
-	}
