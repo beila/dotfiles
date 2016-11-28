16d09cbc385e80c5977b2c5fc8ac44be9f7415d7
Hojin Ghim <beila@kaist.ac.kr>
2016-11-28 16:27:01 +0000

    fix: follow symlinks when scanning LOCAL_ROOT for PATH entries

--- local/path.zsh
+++ local/path.zsh
@@ -2,7 +2,7 @@ LOCAL_ROOT=$HOME/local
 
 if [[ -d $LOCAL_ROOT ]]
 then
-  for src in $(find -H "$LOCAL_ROOT" -maxdepth 1 -type d) $(find -H "$LOCAL_ROOT" -maxdepth 2 -name 'bin' -type d)
+  for src in $(find -L "$LOCAL_ROOT" -maxdepth 1 -type d) $(find -L "$LOCAL_ROOT" -maxdepth 2 -name 'bin' -type d)
   do
     export PATH=$PATH:$src
   done
