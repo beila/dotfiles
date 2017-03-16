9cfa33ba8158beb17e7c8c86f861e673d4bed3a0
Hojin Ghim <beila@kaist.ac.kr>
2017-03-16 11:43:02 +0000

    fix: correct path separator handling in linuxbrew env vars

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,9 +1,9 @@
 if [[ -d $HOME/.linuxbrew ]]
 then
-	export PATH=$HOME/.linuxbrew/sbin:${PATH//$HOME\/.linuxbrew\/sbin}
-	export PATH=$HOME/.linuxbrew/bin${PATH//$HOME\/.linuxbrew\/bin}
-	export MANPATH=$HOME/.linuxbrew/share/man:${MANPATH://$HOME\/.linuxbrew\/share\/man}
-	export INFOPATH=$HOME/.linuxbrew/share/info:${INFOPATH://$HOME\/.linuxbrew\/share\/info}
+	export PATH=$HOME/.linuxbrew/sbin:${PATH//$HOME\/.linuxbrew\/sbin:}
+	export PATH=$HOME/.linuxbrew/bin:${PATH//$HOME\/.linuxbrew\/bin:}
+	export MANPATH=$HOME/.linuxbrew/share/man:${MANPATH://$HOME\/.linuxbrew\/share\/man:}
+	export INFOPATH=$HOME/.linuxbrew/share/info:${INFOPATH://$HOME\/.linuxbrew\/share\/info:}
 fi
 
 # brew info coreutils
