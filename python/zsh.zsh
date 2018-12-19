ba7bfdb4dc4bd4ec0c1e0a84d1ae26554c7b0eeb
Hojin Ghim <beila@kaist.ac.kr>
2018-12-19 15:51:01 +0000

    feat: enable pyenv initialization in zsh config

--- python/zsh.zsh
+++ python/zsh.zsh
@@ -1,18 +1,17 @@
-## https://github.com/pyenv/pyenv#basic-github-checkoutn
-#export PYENV_ROOT="${DOTFILES_ROOT}/python/pyenv"
-#local DIR=${DOTFILES_ROOT}/python/pyenv/bin
-#export PATH=${DIR}:${PATH//${DIR}:}
-#
-#if command -v pyenv 1>/dev/null 2>&1; then
-#	eval "$(pyenv init -)"
-#fi
-#
-## https://github.com/pyenv/pyenv-virtualenv#installing-as-a-pyenv-plugin
-#eval "$(pyenv virtualenv-init -)"
-#
-## https://stackoverflow.com/a/31116425
-#if [ -d "/Library/Python/2.7/site-packages" ]
-#then
-#	export PYTHONPATH="/Library/Python/2.7/site-packages:$PYTHONPATH"
-#fi
+# https://github.com/pyenv/pyenv#basic-github-checkoutn
+export PYENV_ROOT="${DOTFILES_ROOT}/python/pyenv"
+local DIR=${DOTFILES_ROOT}/python/pyenv/bin
+export PATH=${DIR}:${PATH//${DIR}:}
 
+if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init -)"
+fi
+
+# https://github.com/pyenv/pyenv-virtualenv#installing-as-a-pyenv-plugin
+eval "$(pyenv virtualenv-init -)"
+
+# https://stackoverflow.com/a/31116425
+if [ -d "/Library/Python/2.7/site-packages" ]
+then
+	export PYTHONPATH="/Library/Python/2.7/site-packages:$PYTHONPATH"
+fi
