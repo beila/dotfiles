58255c0a46db7982e259ae1428a2ad5e0c3848b4
Hojin Ghim <beila@kaist.ac.kr>
2019-02-04 18:40:02 +0000

    Change from u5a551d87956c59.ant.amazon.com

--- xwindow/xmonad.symlink/xmonad.hs
+++ xwindow/xmonad.symlink/xmonad.hs
@@ -1,7 +1,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
--- import XMonad.Hooks.SetWMName
+import XMonad.Hooks.SetWMName
 import XMonad.Actions.Plane
 
 myManageHook = composeAll
@@ -13,8 +13,8 @@ main = xmonad =<< dzen myConfig
 
 myConfig = defaultConfig
 	-- https://bbs.archlinux.org/viewtopic.php?pid=744577#p744577
-	{ -- startupHook = setWMName "LG3D" ,
-    modMask = mod4Mask
+	{ startupHook = setWMName "LG3D"
+    , modMask = mod4Mask
     -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration#Final_Touches
 	-- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Make_space_for_a_panel_dock_or_tray
 	, manageHook = manageDocks <+> myManageHook
