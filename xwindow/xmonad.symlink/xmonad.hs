e06597827ab0e14b55d1b0153727c2f50f57bd54
Hojin Ghim <beila@kaist.ac.kr>
2019-02-07 13:40:01 +0000

    Change from u5a551d87956c59.ant.amazon.com

--- xwindow/xmonad.symlink/xmonad.hs
+++ xwindow/xmonad.symlink/xmonad.hs
@@ -8,6 +8,11 @@ import XMonad.Util.Run(spawnPipe)
 
 myManageHook = composeAll
     [ className =? "Tilda"  --> doFloat
+    , className =? "Thunderbird" --> doShift "1:mail"
+    , className =? "jetbrains-clion" --> doShift "3:clion"
+    , className =? "yakyak" --> doShift "9:messenger"
+    , className =? "AmazonChime" --> doShift "9:messenger"
+    , className =? "Gvim" --> doShift "4:gvim"
     ]
 
 -- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#dzen_status_bars
@@ -17,6 +22,8 @@ myConfig = defaultConfig
 	-- https://bbs.archlinux.org/viewtopic.php?pid=744577#p744577
 	{ startupHook = setWMName "LG3D"
     , modMask = mod4Mask
+    -- https://wiki.haskell.org/Xmonad/General_xmonad.hs_config_tips#ManageHook_examples
+    , workspaces = ["1:mail", "2:work browser", "3:clion", "4:gvim", "5", "6", "7:browser", "8:calendar", "9:messenger"]
     -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration#Final_Touches
 	-- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Make_space_for_a_panel_dock_or_tray
 	, manageHook = manageDocks <+> myManageHook
@@ -25,4 +32,5 @@ myConfig = defaultConfig
 	} `additionalKeys`
     -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen%27s_Configuration#Customizing_xmonad
     [ ((mod4Mask, xK_l), spawn "gnome-screensaver-command --lock")
+    , ((mod4Mask .|. mod1Mask, xK_o), swapNextScreen)
     ]
