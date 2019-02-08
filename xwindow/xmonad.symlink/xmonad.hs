441d94eba16ce5ca6bb32fd53d4cb6e68f5e4f16
Hojin Ghim <beila@kaist.ac.kr>
2019-02-08 12:40:02 +0000

    refactor(xmonad): extract workspaces and keys, add per-workspace view/shift bindings

--- xwindow/xmonad.symlink/xmonad.hs
+++ xwindow/xmonad.symlink/xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Actions.CycleWS
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Layout.PerWorkspace
+import qualified XMonad.StackSet as W
 
 myManageHook = composeAll
     [ className =? "Tilda"  --> doFloat
@@ -24,16 +25,25 @@ myConfig = defaultConfig
 	{ startupHook = setWMName "LG3D"
     , modMask = mod4Mask
     -- https://wiki.haskell.org/Xmonad/General_xmonad.hs_config_tips#ManageHook_examples
-    , workspaces = ["1:mail", "2:work browser", "3:clion", "4:gvim", "5", "6", "7:browser", "8:calendar", "9:messenger"]
+    , workspaces = myWorkspaces
     -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration#Final_Touches
 	-- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Make_space_for_a_panel_dock_or_tray
 	, manageHook = manageDocks <+> myManageHook
                 <+> manageHook defaultConfig
 	, layoutHook = avoidStruts  $  layoutHook defaultConfig
-	} `additionalKeys`
-    -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen%27s_Configuration#Customizing_xmonad
-    [ ((mod4Mask .|. mod1Mask, xK_l), spawn "gnome-screensaver-command --lock")
+	} `additionalKeys` myKeys
+
+myWorkspaces = ["1:mail", "2:work browser", "3:clion", "4:gvim", "5", "6", "7:browser", "8:calendar", "9:messenger"]
+{-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]-}
+
+-- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen%27s_Configuration#Customizing_xmonad
+myKeys = [ ((mod4Mask .|. mod1Mask, xK_l), spawn "gnome-screensaver-command --lock")
     -- https://hackage.haskell.org/package/xmonad-contrib-0.15/docs/XMonad-Actions-CycleWS.html#v:nextScreen
     , ((mod4Mask, xK_w), nextScreen)
     , ((mod4Mask, xK_0), moveTo Next EmptyWS)  -- find a free workspace
+    ] ++
+    -- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Replacing_greedyView_with_view
+    [ ((m .|. mod4Mask, k), windows $ f i)
+    | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask), (W.greedyView, controlMask)]
     ]
