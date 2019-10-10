3a42a4d3f2af13eddee70fbc5d0e66778dddb517
Hojin Ghim <beila@kaist.ac.kr>
2019-10-10 14:22:02 +0100

    fix: initialize MANPATH and configure git-subrepo paths

--- git/git-subrepo.zsh
+++ git/git-subrepo.zsh
@@ -1,1 +1,7 @@
+export MANPATH=${MANPATH:-$(manpath)} # should be initialised otherwise default values are not used
 source ${DOTFILES_ROOT}/git/git-subrepo/.rc
+
+local dir=${DOTFILES_ROOT}/git/git-subrepo/lib
+path=(${(@)path:#$dir} $dir)
+local dir=${DOTFILES_ROOT}/git/git-subrepo/man
+manpath=(${(@)manpath:#$dir} $dir)
