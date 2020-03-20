57454cd418435e39d253358b430d9c4934d6f492
Hojin Ghim <beila@kaist.ac.kr>
2020-03-20 02:45:43 +0000

    Change from 9801a78c9bc1

---
+++ python/pyenv/shims/activate.fish
@@ -0,0 +1,6 @@
+[ -n "$PYENV_DEBUG" ] && set -x
+export PYENV_ROOT="/Users/hojin/.dotfiles/python/pyenv"
+program="$("/Users/hojin/.dotfiles/python/pyenv/libexec/pyenv" which "${BASH_SOURCE##*/}")"
+if [ -e "${program}" ]; then
+  . "${program}" "$@"
+fi
