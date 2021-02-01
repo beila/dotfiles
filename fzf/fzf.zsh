6c5af0aabe877e132dd1a396afce914aba2cd90e
Hojin Ghim <beila@kaist.ac.kr>
2021-02-01 13:00:47 +0000

    Change from f018986a0003

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,4 +1,5 @@
 echo '================================fzf'
-export FZF_ALT_C_COMMAND="dirs; command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune -o -type d -print 2> /dev/null | cut -b3-"
+export FZF_ALT_C_COMMAND="dirs -lp; command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune -o -type d -print 2> /dev/null | cut -b3-"
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh && bindkey '^E' fzf-cd-widget
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh \
+    && bindkey '^E' fzf-cd-widget
