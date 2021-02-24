63046e17d63fad786b2005c572b71dd0e777a29b
Hojin Ghim <beila@kaist.ac.kr>
2021-02-24 13:59:00 +0000

    feat(fzf): source custom functions and key bindings

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -3,3 +3,7 @@ export FZF_ALT_C_COMMAND="fasd -dlR"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh \
     && bindkey '^E' fzf-cd-widget
+
+local DIR=$(dirname $(readlink -f $0))
+source ${DIR}/functions.sh/functions.sh
+source ${DIR}/functions.sh/key-binding.zsh
