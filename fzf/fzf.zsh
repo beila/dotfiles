b54b84f8d5fbfd0d5e383a1f7eeeac3902b299c6
Hojin Ghim <beila@kaist.ac.kr>
2021-03-23 10:59:00 +0000

    feat(fzf): extend CTRL_T_COMMAND with find fallback

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,4 +1,7 @@
-export FZF_CTRL_T_COMMAND="fasd -alR"
+export FZF_CTRL_T_COMMAND="fasd -alR; command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | cut -b3-"
 export FZF_ALT_C_COMMAND="fasd -dlR"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh \
