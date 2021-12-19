fc274ae7eb38f238f744e383b6cf44499028c064
Hojin Ghim <beila@kaist.ac.kr>
2021-12-19 17:40:50 +0000

    Change from f018986a0003

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -2,7 +2,9 @@ export FZF_CTRL_T_COMMAND="fasd -alR; command find -L . -mindepth 1 \\( -path '*
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | cut -b3-"
+export FZF_CTRL_T_OPTS="--preview 'test -f {} && bat --style=numbers --color=always --line-range :500 {} || exa -l {} || ls -l --color {}'"
 export FZF_ALT_C_COMMAND="fasd -dlR"
+export FZF_ALT_C_OPTS="--preview 'exa -l {} || ls -l --color {}'"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh \
     && bindkey '^E' fzf-cd-widget
