3f896cb15f00da03809d537fa34a11c520b14b94
Hojin Ghim <beila@kaist.ac.kr>
2022-01-10 17:45:01 +0000

    Change from dev-dsk-hojin-1c-1d10d9ca

--- fzf/functions.sh/functions.sh
+++ fzf/functions.sh/functions.sh
@@ -38,7 +38,7 @@ _gh() {
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | Xargs git show --patch-with-stat --color=always' |
+    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | xargs git show --patch-with-stat --color=always' |
   grep -o "[a-f0-9]\{7,\}"
 }
 
