038e3ba80de8a04c910371e654ad0b13fe3b574b
Hojin Ghim <beila@kaist.ac.kr>
2022-01-24 13:13:01 +0000

    Change from f018986a0003

--- brew/zsh.zsh
+++ brew/zsh.zsh
@@ -8,3 +8,5 @@ HELPDIR=/usr/local/share/zsh/help
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+alias brwe=brew
