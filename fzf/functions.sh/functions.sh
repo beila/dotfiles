49dcc8f77366c061ba2d8e4f7ba496bfd0435e90
Hojin Ghim <beila@kaist.ac.kr>
2022-01-26 12:13:01 +0000

    Change from f018986a0003

--- fzf/functions.sh/functions.sh
+++ fzf/functions.sh/functions.sh
@@ -5,14 +5,14 @@ is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
 
-fzf-down() {
+fzf_down() {
   fzf --height 50% --min-height 20 --border --bind ctrl-/:toggle-preview "$@"
 }
 
 _gf() {
   is_in_git_repo || return
   git -c color.status=always status --ignore-submodules=${_git_status_ignore_submodules} --short |
-  fzf-down -m --ansi --nth 2..,.. \
+  fzf_down -m --ansi --nth 2..,.. \
   --preview '(_gf_=$(git diff --cached --color=always HEAD -- {-1} | sed 1,4d; git diff --color=always -- {-1} | sed 1,4d); if [[ "$_gf_" ]]; then echo "$_gf_"; else; cat {-1} ; fi)' |
   cut -c4- | sed 's/.* -> //'
 }
@@ -20,7 +20,7 @@ _gf() {
 _gb() {
   is_in_git_repo || return
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  fzf-down --ansi --multi --tac --preview-window right:70% \
+  fzf_down --ansi --multi --tac --preview-window right:70% \
     --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
   sed 's/^..//' | cut -d' ' -f1 |
   sed 's#^remotes/##'
@@ -29,14 +29,14 @@ _gb() {
 _gt() {
   is_in_git_repo || return
   git tag --sort -version:refname |
-  fzf-down --multi --preview-window right:70% \
-    --preview 'git show --color=always {}'
+  fzf_down --multi --preview-window right:70% \
+    --preview 'git show --patch-with-stat --color=always {}'
 }
 
 _gh() {
   is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+  fzf_down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | xargs git show --patch-with-stat --color=always' |
   grep -o "[a-f0-9]\{7,\}"
@@ -45,13 +45,13 @@ _gh() {
 _gr() {
   is_in_git_repo || return
   git remote -v | awk '{print $1 "\t" $2}' | uniq |
-  fzf-down --tac \
+  fzf_down --tac \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1}' |
   cut -d$'\t' -f1
 }
 
 _gs() {
   is_in_git_repo || return
-  git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
+  git stash list | fzf_down --reverse -d: --preview 'git show --patch-with-stat --color=always {1}' |
   cut -d: -f1
 }
