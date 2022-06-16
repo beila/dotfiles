88a934acd7a62687f27fbfd2ea363745a0322b5d
Hojin Ghim <beila@kaist.ac.kr>
2022-06-16 19:28:01 +0100

    feat(fzf): include git tracked files in CTRL-T command

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,4 +1,7 @@
-export FZF_CTRL_T_COMMAND="fasd -alR; command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+export FZF_CTRL_T_COMMAND="fasd -alR;\
+    git ls-files --cached;\
+    
+    command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
     -o -type f -print \
     -o -type d -print \
     -o -type l -print 2> /dev/null | cut -b3-"
