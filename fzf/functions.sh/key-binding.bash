c7c503adbf20f2fd9f826e5ec9ef0dd8faa0f4e0
Hojin Ghim <beila@kaist.ac.kr>
2022-08-05 17:14:01 +0900

    chore: remove unused gq fzf git log function

--- fzf/functions.sh/key-binding.bash
+++ fzf/functions.sh/key-binding.bash
@@ -4,7 +4,6 @@ if [[ $- =~ i ]]; then
   bind '"\C-g\C-b": "$(_gb)\e\C-e\er"'
   bind '"\C-g\C-t": "$(_gt)\e\C-e\er"'
   bind '"\C-g\C-h": "$(_gh)\e\C-e\er"'
-  bind '"\C-g\C-g": "$(_gq)\e\C-e\er"'
   bind '"\C-g\C-y": "$(_gy)\e\C-e\er"'
   bind '"\C-g\C-r": "$(_gr)\e\C-e\er"'
   bind '"\C-g\C-s": "$(_gs)\e\C-e\er"'
