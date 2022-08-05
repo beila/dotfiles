f4b361a3e4512f46bdc9a71dcc50f6bb6531d817
Hojin Ghim <beila@kaist.ac.kr>
2022-08-05 17:14:01 +0900

    Change from ITVB-5CG14202W4

--- fzf/functions.sh/functions.sh
+++ fzf/functions.sh/functions.sh
@@ -42,15 +42,6 @@ _gh() {
   grep -o "[a-f0-9]\{7,\}"
 }
 
-_gq() {
-  is_in_git_repo || return
-  git log --all --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf_down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header 'Press CTRL-S to toggle sort' \
-    --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | head -1 | xargs git show --patch-with-stat --color=always' |
-  grep -o "[a-f0-9]\{7,\}"
-}
-
 _gy() {
   is_in_git_repo || return
   git reflog --color=always |
