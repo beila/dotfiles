4f40d70b3eb0a9b258f390fc91317890307ca8d0
Hojin Ghim <beila@kaist.ac.kr>
2022-11-11 11:05:30 +0000

    chore: update WSL config and DNS resolution scripts

--- git/git.zsh
+++ git/git.zsh
@@ -4,7 +4,7 @@ if [ "$?" -eq 0 ]
 then
     setx.exe GIT_SSH 'C:\Windows\System32\OpenSSH\ssh.exe' > /dev/null
     function git() {
-        #rm -v $(wslpath "$(git rev-parse --git-dir)/index.lock") 1>&2
+        #rm -v $(wslpath "$(git.exe rev-parse --git-dir)/index.lock") 1>&2
         git.exe "$@"
     }
 fi
