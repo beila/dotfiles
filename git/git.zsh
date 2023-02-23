037e370bc63fa2314e809f9356e2dfa67a4e88aa
Hojin Ghim <beila@kaist.ac.kr>
2023-02-23 15:46:29 +0000

    Use existing worktree from gWc even with a different branch name

--- git/git.zsh
+++ git/git.zsh
@@ -3,9 +3,9 @@ which git.exe 2> /dev/null 1>&2
 if [ "$?" -eq 0 ]
 then
     setx.exe GIT_SSH 'C:\Windows\System32\OpenSSH\ssh.exe' > /dev/null
-    function git() {
-        #rm -v $(wslpath "$(git.exe rev-parse --git-dir)/index.lock") 1>&2
-        git.exe "$@"
-    }
+    #function git() {
+        ##rm -v $(wslpath "$(git.exe rev-parse --git-dir)/index.lock") 1>&2
+        #git.exe "$@"
+    #}
 fi
 
