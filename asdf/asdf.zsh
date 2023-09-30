5ac5452816fdc5329f514f5c24fad6912366bda4
Hojin Ghim <beila@kaist.ac.kr>
2023-09-30 21:13:01 +0100

    Change from f018986a0003

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,1 +1,2 @@
 [ -s "/home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh"
+[ -s "/usr/local/opt/asdf/libexec/asdf.sh" ] && . "/usr/local/opt/asdf/libexec/asdf.sh"
