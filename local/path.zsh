7aca1b9249b95767efe649adbefa57acc9227620
Hojin Ghim <beila@kaist.ac.kr>
2023-10-19 13:26:01 +0100

    chore: expand path search depth and make docker_here home optional

--- local/path.zsh
+++ local/path.zsh
@@ -4,7 +4,7 @@ for local_root in $LOCAL_ROOTS
 do
     if [[ -d $local_root ]]
     then
-        for dir in $(find -L "$local_root" -maxdepth 1 -type d) $(find -L "$local_root" -maxdepth 2 -name 'bin' -type d)
+        for dir in $(find -L "$local_root" -maxdepth 1 -type d) $(find -L "$local_root" -maxdepth 3 -name 'bin' -type d)
         do
             path=($dir ${(@)path:#$dir})
         done
