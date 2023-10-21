b58794efa3c0b16dde1e7ee026091785c14615d6
Hojin Ghim <beila@kaist.ac.kr>
2023-10-21 13:26:02 +0100

    Change from u7793aaa93ef05f

--- git/git-subrepo.zsh
+++ git/git-subrepo.zsh
@@ -1,6 +1,6 @@
-source ${DOTFILES_ROOT}/git/git-subrepo/.rc
+#source ${DOTFILES_ROOT}/git/git-subrepo/.rc
 
 local dir=${DOTFILES_ROOT}/git/git-subrepo/lib
 path=(${(@)path:#$dir} $dir)
 local dir=${DOTFILES_ROOT}/git/git-subrepo/man
-manpath=(${(@)manpath:#$dir} $dir)
+#manpath=(${(@)manpath:#$dir} $dir)
