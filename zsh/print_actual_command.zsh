28578b7c007c39686b244fc9efd5776513bceeda
Hojin Ghim <beila@kaist.ac.kr>
2023-11-06 16:26:01 +0000

    Change from u7793aaa93ef05f

--- zsh/print_actual_command.zsh
+++ zsh/print_actual_command.zsh
@@ -1,2 +1,5 @@
 # print the actual command whenever a command starts, if it's different from the typed command
-add-zsh-hook -Uz preexec (){[ "$1" = "$3" ] || echo "$PS4[1,1] $3"}
+function print_actual_command (){
+    [ "$1" = "$3" ] || echo "$PS4[1,1] $3"
+}
+add-zsh-hook -Uz preexec print_actual_command
