cc3cc3813d277a8210219c0154c5bf8f31b61530
Hojin Ghim <beila@kaist.ac.kr>
2023-11-13 17:26:01 +0000

    Change from u7793aaa93ef05f

--- nvim.configsymlink/lua/options.lua
+++ nvim.configsymlink/lua/options.lua
@@ -37,6 +37,6 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+-- vim.o.termguicolors = true
 
 -- vim: ts=2 sts=2 sw=2 et
