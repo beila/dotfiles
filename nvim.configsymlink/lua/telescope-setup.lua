3988603ec1d6398a793dcae36101203f294b4457
Hojin Ghim <beila@kaist.ac.kr>
2023-11-14 17:26:01 +0000

    feat: reorganize telescope keymaps and add LSP/git pickers

--- nvim.configsymlink/lua/telescope-setup.lua
+++ nvim.configsymlink/lua/telescope-setup.lua
@@ -41,7 +41,7 @@ local function find_git_root()
   return git_root
 end
 
--- Custom live_grep function to search in git root
+--[[ -- Custom live_grep function to search in git root
 local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
@@ -51,8 +51,8 @@ local function live_grep_git_root()
   end
 end
 
-vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
-
+-- vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
+]]
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>b', require('telescope.builtin').oldfiles, { desc = '[ ] Find recently opened files' })
 vim.keymap.set('n', '<leader>\'', function()
@@ -63,14 +63,12 @@ vim.keymap.set('n', '<leader>\'', function()
   } ]])
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files, { desc = 'Search Git [F]iles' })
+vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files, { desc = 'Search [F]iles including ignored ones' })
 vim.keymap.set('n', '<leader>h', require('telescope.builtin').help_tags, { desc = 'Search [H]elp' })
 vim.keymap.set('n', '<leader>H', require('telescope.builtin').man_pages, { desc = 'Manual Pages' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set({'n', 'v'}, '<leader>g', require('telescope.builtin').grep_string, { desc = 'Search Current Word' })
+vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = 'Search [D]iagnostics' })
 
 vim.keymap.set('n', '<leader>s', require('telescope.builtin').git_status, { desc = 'Git [S]tatus' })
 vim.keymap.set('n', '<leader>t', require('telescope.builtin').builtin, { desc = '[T]elescope Builtins' })
@@ -78,6 +76,34 @@ vim.keymap.set('n', '<leader>q', require('telescope.builtin').quickfix, { desc =
 vim.keymap.set('n', '<leader>Q', require('telescope.builtin').quickfixhistory, { desc = '[Q]uickfix history' })
 vim.keymap.set('n', '<leader>;', require('telescope.builtin').command_history, { desc = 'Command History' })
 vim.keymap.set('n', '<leader>/', require('telescope.builtin').search_history, { desc = 'Search History' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').marks, { desc = 'Vim marks' })
+vim.keymap.set('n', '<leader>r', require('telescope.builtin').spell_suggest, { desc = '[R]ecommend spelling for the current word' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').jumplist, { desc = 'Jump list' })
+
+vim.keymap.set('n', '<leader>cr', require('telescope.builtin').lsp_references, { desc = '[C]ode [R]eferences' })
+vim.keymap.set('n', '<leader>ci', require('telescope.builtin').lsp_incoming_calls, { desc = '[C]ode [I]ncoming calls' })
+vim.keymap.set('n', '<leader>co', require('telescope.builtin').lsp_outgoing_calls, { desc = '[C]ode [O]outgoing calls' })
+vim.keymap.set('n', '<leader>cd', require('telescope.builtin').lsp_definitions, { desc = '[C]ode [D]efinitions' })
+vim.keymap.set('n', '<leader>ct', require('telescope.builtin').lsp_type_definitions, { desc = '[C]ode [T]ype Definitions' })
+vim.keymap.set('n', '<leader>cm', require('telescope.builtin').lsp_implementations, { desc = '[C]ode I[m]plementations' })
+vim.keymap.set('n', '<leader>cl', require('telescope.builtin').lsp_document_symbols, { desc = '[C]ode [L]ocal symbols' })
+vim.keymap.set('n', '<leader>cs', require('telescope.builtin').lsp_workspace_symbols, { desc = '[C]ode Workspace [S]ymbols' })
+vim.keymap.set('n', '<leader>cw', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[C]ode [W]orkspace symbols dynamically' })
+
+vim.keymap.set('n', '<leader>l', require('telescope.builtin').git_bcommits, { desc = 'Git [L]og for buffer' })
+require("telescope").setup {
+  pickers = {
+    git_bcommits = {
+      mappings = {
+        i = {
+          -- It checks out the current file from the selected commit and drop all the current changes!
+          ["<CR>"] = false,
+        },
+        n = {
+          ["<CR>"] = false,
+        },
+      },
+    },
+  },
+}
 
 -- vim: ts=2 sts=2 sw=2 et
