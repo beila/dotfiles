2cfe675916de5909f58a114f6ac2d696836291ce
Hojin Ghim <beila@kaist.ac.kr>
2024-01-31 14:26:01 +0000

    Change from u7793aaa93ef05f

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,2 +1,6 @@
+ASDF_FORCE_PREPEND=yes
 [ -s "/home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh"
 [ -s "/usr/local/opt/asdf/libexec/asdf.sh" ] && . "/usr/local/opt/asdf/libexec/asdf.sh"
+
+# I need venv before asdf
+. $(dirname "$0")/../python/path.zsh
