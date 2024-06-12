77b71c717e1932c2aa14269f668fc3bb1334fdbe
Hojin Ghim <beila@kaist.ac.kr>
2024-06-12 12:59:36 +0100

    git subrepo pull gdb/GEP
    subrepo:
      subdir:   "gdb/GEP"
      merged:   "691ea6df"
    upstream:
      origin:   "https://github.com/lebr0nli/GEP.git"
      branch:   "main"
      commit:   "691ea6df"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

--- gdb/GEP/install.sh
+++ gdb/GEP/install.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-set -ex
 
-# create a folder for GEP
-INSTALL_PATH=${XDG_DATA_HOME:-$HOME/.local/share}/GEP
-mkdir -p "$INSTALL_PATH"
-GDBINIT_GEP_PY=$INSTALL_PATH/gdbinit-gep.py
-echo "Installing GEP to $INSTALL_PATH ..."
+set -o errexit
 
-if [ -d "$INSTALL_PATH/.git" ]; then
-    # git pull if exists
-    cd "$INSTALL_PATH"
-    git pull
-else
-    # git clone the repo if not exists
-    git clone https://github.com/lebr0nli/GEP.git --depth=1 "$INSTALL_PATH"
+help_and_exit() {
+    echo "Usage: $0 [-d|--dev]"
+    cat << EOF
+  -d,  --dev         install development dependencies
+EOF
+    exit 1
+}
+
+if [[ $# -gt 1 ]]; then
+    help_and_exit
 fi
+
+DEV=0
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -d | --dev)
+            DEV=1
+            shift
+            ;;
+        *)
+            help_and_exit
+            ;;
+    esac
+done
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+GEP_BASE=$(pwd)
+GDBINIT_GEP_PY=$GEP_BASE/gdbinit-gep.py
+echo "GEP installation path: $GEP_BASE"
 
 # find python path
 PYVER=$(gdb -batch -q --nx -ex 'pi import platform; print(".".join(platform.python_version_tuple()[:2]))')
@@ -24,19 +41,27 @@ if ! uname -a | grep -q Darwin > /dev/null; then
 fi
 
 # create venv and install prompt_toolkit
-VENV_PATH=$INSTALL_PATH/.venv
+VENV_PATH=$GEP_BASE/.venv
 echo "Creating virtualenv in path: ${VENV_PATH}"
 "$PYTHON" -m venv "$VENV_PATH"
 PYTHON=$VENV_PATH/bin/python
+echo "Installing dependencies"
 "$PYTHON" -m pip install -U pip
-"$VENV_PATH/bin/pip" install --no-cache-dir -U prompt_toolkit
+if [[ $DEV == 1 ]]; then
+    poetry install --with dev
+else
+    "$VENV_PATH/bin/pip" install --no-cache-dir -e .
+fi
 
-# copy example config to INSTALL_PATH if not exists
-cp -n "$INSTALL_PATH"/example/* "$INSTALL_PATH"
+# copy example config to GEP_BASE if not exists
+echo "Copying default config to $GEP_BASE if not exists"
+cp -n "$GEP_BASE"/example/* "$GEP_BASE"
 
-# append gep to gdbinit
+# append GEP to gdbinit if not exists
 if ! grep -q '^[^#]*source.*/gdbinit-gep.py' ~/.gdbinit; then
-    printf '\nsource%s\n' "$GDBINIT_GEP_PY" >> ~/.gdbinit
+    echo "Appending GEP to ~/.gdbinit"
+    printf '\n# Please make sure the following line is always the last in this file\n' >> ~/.gdbinit
+    printf 'source %s\n' "$GDBINIT_GEP_PY" >> ~/.gdbinit
 fi
 
 exit 0
