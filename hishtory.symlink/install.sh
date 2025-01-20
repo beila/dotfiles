cfbf099f1053b98818e9d17c6dce6e1ea89a6df1
Hojin Ghim <beila@kaist.ac.kr>
2025-01-20 14:42:01 +0000

    Change from dev-dsk-hojin-1c-1d10d9ca

--- hishtory.symlink/install.sh
+++ hishtory.symlink/install.sh
@@ -1,2 +1,5 @@
+# shellcheck disable=SC2016
+
 curl https://hishtory.dev/install.py | python3 -
-#hishtory init $YOUR_HISHTORY_SECRET
+echo 'run "hishtory init $YOUR_HISHTORY_SECRET"'
+echo 'where $YOUR_HISHTORY_SECRET is shown by "hishtory status" on a already setup machine.'
