cbb281a2252e422fc0411a386ee9fcac35e22d9e
Hojin Ghim <hojin@amazon.com>
2025-03-31 15:18:01 +0100

    feat(nvim-amazon): add Barium LSP, Bemol workspace, and GitFarm fugitive support

---
+++ nvim-amazon/plugin/init.lua
@@ -0,0 +1,140 @@
+-- https://w.amazon.com/bin/view/Users/Ethdestr/vim/#HNeovimLocalConfiguration
+
+-- Only load when not in vscode
+if vim.g.vscode then
+  return
+end
+
+-- [[ Barium support ]]
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig.configs')
+
+-- https://w.amazon.com/bin/view/Barium/#HVimandNeovim
+-- Remove hyphen because it breaks vim/neovim
+vim.filetype.add({ filename = { Config = "brazilconfig" } })
+
+-- Check if the config is already defined (useful when reloading this file)
+if not configs.barium then
+  configs.barium = {
+    default_config = {
+      cmd = { 'barium' },
+      filetypes = { 'brazilconfig' },
+      root_dir = function(fname)
+        return lspconfig.util.find_git_ancestor(fname)
+      end,
+      settings = {},
+    },
+  }
+end
+
+lspconfig.barium.setup({})
+
+-- [[ Add support for Bemol folders ]]
+-- https://w.amazon.com/bin/view/Bemol/#HnvimbuiltinLSP28withlsp-configand2Forlsp-zero29
+local function bemol()
+  -- Find bemol directory based on current working directory, not parent directory of current buffer.
+  local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory' })[1]
+  local ws_folders_lsp = {}
+  if not bemol_dir then
+    vim.notify(".bemol directory not found! Not in a Brazil workspace?", vim.log.levels.WARN)
+  else
+    vim.notify(".bemol directory found: " .. bemol_dir, vim.log.levels.INFO)
+    local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+    if file then
+      for line in file:lines() do
+        table.insert(ws_folders_lsp, line)
+      end
+      file:close()
+    end
+  end
+
+  for _, line in ipairs(ws_folders_lsp) do
+    vim.lsp.buf.add_workspace_folder(line)
+  end
+
+  return ws_folders_lsp
+end
+
+-- Should be rarely needed because of auto command defined below
+vim.api.nvim_create_user_command(
+  'BemolAdditions',
+  bemol,
+  {
+    desc = 'Add bemol detected workspace folders',
+  }
+)
+
+vim.api.nvim_create_autocmd('LspAttach',
+  {
+    -- Should stack with existing autogroup for defining keymaps, etc.
+    group = vim.api.nvim_create_augroup('UserLspConfig', { clear = false }),
+
+    -- bemol supports multiple workspace roots for Java, Python, and Ruby Brazil workspaces.
+    -- https://w.amazon.com/bin/view/Bemol/#HBrazilPlugins
+    pattern = { '*.java', '*.py', '*.rb' },
+
+    callback = function(args)
+      bemol()
+    end,
+  }
+)
+
+-- [[ :GBrowse support for Amazon internal code.amazon.com website ]]
+-- copy/pasted from here: https://w.amazon.com/bin/view/Users/Btaine/vim#Fugitive_integration_with_Code_Browser
+-- modified version here: https://w.amazon.com/bin/view/Users/Ethdestr/vim/#HFugitiveintegrationwithCodeBrowser
+vim.cmd([[
+" ============================================================================
+" File:        fugitive-gitfarm.vim
+" Maintainer:  Benoît Taine <btaine@amazon.com>
+" ============================================================================
+
+function! s:function(name) abort
+  return function(substitute(a:name,'^s:',matchstr(expand('<sfile>'), '<SNR>\d\+_'),''))
+endfunction
+
+function! s:gitfarm_url(opts) abort
+  if a:0 || type(a:opts) != type({})
+    return ''
+  endif
+  let default_domain = 'git\.amazon\.com'
+  let domain = exists('g:fugitive_gitfarm_domain') ? g:fugitive_gitfarm_domain : default_domain
+  let repo = matchstr(get(a:opts, 'remote'), '^ssh://\(\w\+@\)\?' . domain . '\(:\d\+\)\?/pkg/\w\+$')
+  if repo ==# ''
+    return ''
+  endif
+  let package = split(repo, '/')[4]
+  let path = substitute(a:opts.path, '^/', '', '')
+  let root = 'https://code.amazon.com/packages/' . package
+  if path =~# '^\.git/\%(config$\|hooks\>\)'
+    return root . '/repo-info'
+  elseif path =~# '^\.git\>'
+    return root
+  endif
+  if a:opts.commit =~# '^\d\=$'
+    let commit = a:opts.repo.rev_parse('HEAD')
+  else
+    let commit = a:opts.commit
+  endif
+  if get(a:opts, 'type', '') ==# 'tree' || a:opts.path =~# '/$'
+    let url = substitute(root . '/trees/' . commit . '/--/' . path, '/$', '', 'g')
+  elseif get(a:opts, 'type', '') ==# 'blob' || a:opts.path =~# '[^/]$'
+    let url = root . '/blobs/' . commit . '/--/' . path
+    if get(a:opts, 'line2')
+      let url .= '#L' . a:opts.line1
+      " Linking a range of lines
+      if a:opts.line1 != a:opts.line2
+        let url .= '-L' . a:opts.line2
+      endif
+    endif
+  else
+    let url = root . '/commits/' . commit
+  endif
+  return url
+endfunction
+
+if !exists('g:fugitive_browse_handlers')
+  let g:fugitive_browse_handlers = []
+endif
+
+call insert (g:fugitive_browse_handlers, s:function('s:gitfarm_url'))
+]])
