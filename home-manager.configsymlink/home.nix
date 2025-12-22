63073ebe62e6190bab55a59975a2a4e142d666bc
Hojin Ghim <beila@kaist.ac.kr>
2025-12-22 16:51:05 +0000

    Change from u7793aaa93ef05f

--- home-manager.configsymlink/home.nix
+++ home-manager.configsymlink/home.nix
@@ -31,8 +31,6 @@ in
         pkgs.cargo
         pkgs.mergiraf
 
-        pkgs.ccache
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
