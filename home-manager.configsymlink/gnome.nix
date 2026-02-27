c84515b18423141ee910336ef5a24a77dfc111d1
Hojin Ghim <beila@kaist.ac.kr>
2026-02-27 14:56:05 +0000

    feat: add ibus-hangul 3-bulsik input source and system deps script

--- home-manager.configsymlink/gnome.nix
+++ home-manager.configsymlink/gnome.nix
@@ -3,6 +3,14 @@
   dconf.settings = {
     "org/gnome/desktop/input-sources" = {
       xkb-options = [ "ctrl:nocaps" ];
+      sources = [
+        (lib.gvariant.mkTuple [ "xkb" "us" ])
+        (lib.gvariant.mkTuple [ "ibus" "hangul" ])
+      ];
+    };
+    # ibus-hangul: use 3-bulsik layout (requires: sudo apt install ibus-hangul)
+    "org/freedesktop/ibus/engine/hangul" = {
+      hangul-keyboard = "3f";
     };
     "org/gnome/desktop/peripherals/keyboard" = {
       delay = lib.gvariant.mkUint32 200;
