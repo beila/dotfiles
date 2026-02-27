ec17f83a124f47dd4f8024e8b223fbd6bc3d12ec
Hojin Ghim <hojin@amazon.com>
2026-02-27 13:16:07 +0000

    Change from u9d33b3caf18c58

--- home-manager.configsymlink/gnome.nix
+++ home-manager.configsymlink/gnome.nix
@@ -1,12 +1,12 @@
-{ ... }:
+{ lib, ... }:
 {
   dconf.settings = {
     "org/gnome/desktop/input-sources" = {
       xkb-options = [ "ctrl:nocaps" ];
     };
     "org/gnome/desktop/peripherals/keyboard" = {
-      delay = 100;
-      repeat-interval = 10;
+      delay = lib.gvariant.mkUint32 200;
+      repeat-interval = lib.gvariant.mkUint32 10;
     };
   };
 }
