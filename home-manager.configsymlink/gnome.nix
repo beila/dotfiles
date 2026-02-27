b5f911f85768d2026845085c867b46dce54c6047
Hojin Ghim <beila@kaist.ac.kr>
2026-02-27 12:36:06 +0000

    feat: configure faster keyboard repeat rate in gnome

--- home-manager.configsymlink/gnome.nix
+++ home-manager.configsymlink/gnome.nix
@@ -4,5 +4,9 @@
     "org/gnome/desktop/input-sources" = {
       xkb-options = [ "ctrl:nocaps" ];
     };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = 100;
+      repeat-interval = 10;
+    };
   };
 }
