d229275a08c05c37f89944aa863a264eee0ddfff
Hojin Ghim <--verbose>
2026-02-28 23:56:05 +0000

    Change from u9d33b3caf18c58

---
+++ home-manager.configsymlink/neovide.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+{
+  home.packages = [
+    (config.lib.nixGL.wrap pkgs.neovide)
+    pkgs.jetbrains-mono
+  ];
+
+  xdg.desktopEntries.neovide = {
+    name = "Neovide";
+    comment = "No Nonsense Neovim Client in Rust";
+    # login shell needed so GNOME can find nix-installed neovide and nvim
+    exec = "/bin/bash -lc 'neovide %F'";
+    icon = "neovide";
+    type = "Application";
+    categories = [ "Utility" "TextEditor" ];
+  };
+
+  # Copy nix-installed fonts to ~/.local/share/fonts so neovide's skia
+  # renderer can find them (it doesn't read nix font paths)
+  home.activation.copyFonts = ''
+    mkdir -p ~/.local/share/fonts
+    cp -u ${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-*.ttf ~/.local/share/fonts/ 2>/dev/null || true
+    fc-cache -f 2>/dev/null || true
+  '';
+}
