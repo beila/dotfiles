9a5be6e3ba9ec20ee2509b4ba072f9b092cbaa00
Hojin Ghim <--verbose>
2026-03-02 23:06:05 +0000

    Change from u9d33b3caf18c58

--- home-manager.configsymlink/home.nix
+++ home-manager.configsymlink/home.nix
@@ -43,6 +43,7 @@ in
         pkgs.shellcheck
         pkgs.wl-clipboard
         pkgs.xclip
+        pkgs.xournalpp
         pkgs.xcape  # makes Super tap emit F13 for albert toggle
         pkgs.zoxide
         pkgs.zsh
