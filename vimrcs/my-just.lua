6bee66c9e245605476af1925a5504d2332c342e5
Hojin Ghim <hojin@amazon.com>
2026-03-16 12:05:14 +0000

    Change from taygeta

--- vimrcs/my-just.lua
+++ vimrcs/my-just.lua
@@ -11,5 +11,10 @@ vim.api.nvim_create_autocmd('FileType', {
             cmd = { 'just-lsp' },
             root_dir = vim.fs.root(0, { 'justfile', '.justfile', '.git' }),
         })
+        -- Formatter: just --fmt (overrides <leader>af from lsp.lua)
+        vim.keymap.set({ 'n', 'v' }, '<leader>af', function()
+            vim.cmd('silent !just --fmt --justfile ' .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0)))
+            vim.cmd('edit')
+        end, { buffer = true })
     end,
 })
