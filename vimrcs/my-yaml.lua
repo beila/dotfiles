36146cb108563437e11aff47121ac56dc32b2bcb
Hojin Ghim <hojin@amazon.com>
2026-03-16 14:02:37 +0000

    Change from taygeta

---
+++ vimrcs/my-yaml.lua
@@ -0,0 +1,7 @@
+-- YAML: LSP, formatter
+-- Tools installed via nix in nvim.nix:
+--   yaml-language-server (yamlls), prettier
+-- No DAP or standalone linter (yamlls provides diagnostics)
+
+-- LSP: yamlls (yaml-language-server)
+require('lspconfig').yamlls.setup({})
