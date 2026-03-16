9a8e337e7d33b53175b1bd8860d54cbe3cbe26e1
Hojin Ghim <hojin@amazon.com>
2026-03-16 12:35:18 +0000

    Change from taygeta

---
+++ vimrcs/nvim-lint.lua
@@ -0,0 +1,16 @@
+-- nvim-lint: run external linters as neovim diagnostics
+-- Only linters NOT already handled by their LSP are listed here
+-- (e.g. bashls already runs shellcheck, HLS already runs hlint)
+
+require('lint').linters_by_ft = {
+    cmake = { 'cmake_lint' },
+    dockerfile = { 'hadolint' },
+    java = { 'checkstyle' },
+    make = { 'checkmake' },
+    markdown = { 'markdownlint' },
+    nix = { 'statix', 'deadnix' },
+}
+
+vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
+    callback = function() require('lint').try_lint() end,
+})
