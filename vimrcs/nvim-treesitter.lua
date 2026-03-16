af1ca280d05a057c5031a9fe4c9f6503b8f80c8c
Hojin Ghim <hojin@amazon.com>
2026-03-16 14:02:44 +0000

    Change from taygeta

--- vimrcs/nvim-treesitter.lua
+++ vimrcs/nvim-treesitter.lua
@@ -1,5 +1,19 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "awk", "bash", "c", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "htmldjango", "idl", "java", "javascript", "jinja", "jsdoc", "json", "json5", "jsonc", "just", "kotlin", "lua", "make", "markdown", "markdown_inline", "nim", "ninja", "nix", "passwd", "perl", "printf", "proto", "python", "regex", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
+    ensure_installed = {
+        -- languages
+        "awk", "bash", "c", "cmake", "cpp", "css", "csv", "diff", "dockerfile",
+        "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit",
+        "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "htmldjango",
+        "idl", "java", "javascript", "jinja", "json", "json5", "jsonc", "just",
+        "kotlin", "lua", "make", "markdown", "nim", "ninja", "nix", "passwd",
+        "perl", "proto", "python", "rst", "rust", "sql", "ssh_config", "toml",
+        "typescript", "udev", "vim", "vimdoc", "xml", "yaml",
+        -- injected/inline (not auto-installed)
+        "angular", "asm", "coffeescript", "comment", "fish", "glimmer", "graphql",
+        "haskell_persistent", "jinja_inline", "jsdoc", "latex", "luadoc", "luap",
+        "markdown_inline", "nim_format_string", "pod", "printf", "promql", "query",
+        "re2c", "readline", "regex", "ruby", "slint", "styled",
+    },
     auto_install = true,
     highlight = { enable = true, },
     indent = { enable = true, },
