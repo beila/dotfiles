8ed5149ba6878768e0cb67558ad7fa7f80042564
Hojin Ghim <hojin@amazon.com>
2026-03-16 13:51:35 +0000

    Change from taygeta

--- vimrcs/nvim-treesitter.lua
+++ vimrcs/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "awk", "bash", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "idl", "javascript", "json", "json5", "kotlin", "lua", "make", "markdown", "markdown_inline", "passwd", "perl", "printf", "proto", "python", "regex", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
+    ensure_installed = { "awk", "bash", "c", "cmake", "cpp", "css", "csv", "diff", "dockerfile", "dot", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "glsl", "gnuplot", "groovy", "haskell", "html", "htmldjango", "idl", "java", "javascript", "jinja", "jsdoc", "json", "json5", "jsonc", "just", "kotlin", "lua", "make", "markdown", "markdown_inline", "nim", "ninja", "nix", "passwd", "perl", "printf", "proto", "python", "regex", "rst", "rust", "sql", "ssh_config", "toml", "typescript", "udev", "vim", "vimdoc", "xml", "yaml" },
     auto_install = true,
     highlight = { enable = true, },
     indent = { enable = true, },
