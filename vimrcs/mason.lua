24d7a63557ff4b1dd528eea2f01806f26fe39515
Hojin Ghim <beila@kaist.ac.kr>
2026-03-16 11:26:28 +0000

    chore: remove default mason package installations

--- vimrcs/mason.lua
+++ vimrcs/mason.lua
@@ -6,25 +6,6 @@ require('mason').setup({})
 
 require('mason-lspconfig').setup({
     ensure_installed = {
-        "cmake",           -- cmake
-        "dockerls",        -- docker
-        "docker_compose_language_service", -- docker-compose
-        "glsl_analyzer",   -- glsl/opengl
-        "hls",             -- haskell
-        "html",            -- html/jinja/nunjucks
-        "jsonls",          -- json
-        "ts_ls",           -- javascript/jsx/typescript
-        "jqls",            -- jq
-        "kotlin_language_server", -- kotlin
-        "marksman",        -- markdown
-        "nimls",           -- nim
-        "nil_ls",          -- nix
-        "pyright",         -- python
-        "rust_analyzer",   -- rust
-        "sqlls",           -- sql
-        "taplo",           -- toml
-        "vimls",           -- vimscript
-        "lua_ls",          -- lua
     },
     handlers = {
         function(server_name)
@@ -41,34 +22,5 @@ require("mason-tool-installer").setup({
         -- DAP
         "bash-debug-adapter",    -- bash (DAP config in my-zsh.lua)
         "codelldb",              -- c/c++/rust (DAP config in nvim-dap.lua)
-        "debugpy",               -- python
-        "haskell-debug-adapter", -- haskell
-        "js-debug-adapter",      -- javascript/typescript
-        "kotlin-debug-adapter",  -- kotlin
-
-        -- Linters
-        "hadolint",        -- docker
-        "eslint_d",        -- javascript/jsx/typescript
-        "jsonlint",        -- json
-        "ktlint",          -- kotlin
-        "checkmake",       -- makefile
-        "markdownlint",    -- markdown
-        "statix",          -- nix
-        "ruff",            -- python
-        "sqlfluff",        -- sql
-        "luacheck",        -- lua
-        "vale",            -- text/markdown prose
-
-        -- Formatters
-        "cmake-format",    -- cmake
-        "fourmolu",        -- haskell
-        "prettier",        -- html/json/jsx/js/ts/markdown/jinja/nunjucks
-        "jq",              -- jq/json
-        "ktlint",          -- kotlin
-        "nixpkgs-fmt",     -- nix
-        "ruff",            -- python
-        "rustfmt",         -- rust
-        "sql-formatter",   -- sql
-        "stylua",          -- lua
     },
 })
