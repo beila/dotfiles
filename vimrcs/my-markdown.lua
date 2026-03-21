2129948272333ba2013d05f7bf1c714c9a8dca22
Hojin Ghim <beila@kaist.ac.kr>
2026-03-21 12:50:08 +0000

    auto-enable vim-table-mode for markdown files

--- vimrcs/my-markdown.lua
+++ vimrcs/my-markdown.lua
@@ -3,3 +3,9 @@
 -- Formatter: prettier — installed for html
 
 require('lspconfig').marksman.setup({})
+
+-- 테이블 편집이 편하도록 마크다운에서 자동 활성화 (<leader>tm 으로 토글)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    command = "TableModeEnable",
+})
