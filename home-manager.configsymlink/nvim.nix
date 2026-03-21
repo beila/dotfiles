52ddcb42828bf4c8a3412de8bb3cfa8459dd0653
Hojin Ghim <beila@kaist.ac.kr>
2026-03-21 12:42:39 +0000

    nvim.nix: add vim-table-mode, vim-log-highlighting (buildVimPlugin)

--- home-manager.configsymlink/nvim.nix
+++ home-manager.configsymlink/nvim.nix
@@ -48,9 +48,20 @@ in
       vim-jinja
       vim-just
       vim-matchup
+      vim-table-mode
       vimproc
       vim-rhubarb
       YankRing-vim
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "vim-log-highlighting";
+        version = "1.0.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "MTDL9";
+          repo = "vim-log-highlighting";
+          rev = "1037e26f3120e6a6a2c0c33b14a84336dee2a78f";
+          hash = "sha256-DqYSCtndUNIZsd9zpTFBhESXw3graNrjGC3WhcZ9uTA=";
+        };
+      })
     ];
   };
 
