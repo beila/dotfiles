363c7364408ab7093a1dbc0ad297def7bfd027f0
Hojin Ghim <hojin@amazon.com>
2026-03-21 12:41:26 +0000

    ftdetect: add log-*.txt as log filetype

--- vimrcs/my-text.lua
+++ vimrcs/my-text.lua
@@ -2,3 +2,8 @@
 -- Tools installed via nix in nvim.nix: vale
 -- No LSP, DAP, or formatter for plain text
 -- vale linting configured in nvim-lint.lua
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "log-*.txt",
+    command = "setlocal filetype=log",
+})
