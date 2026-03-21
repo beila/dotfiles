a967ffb194104078b3bb94d4dda85f6b49fe70e3
Hojin Ghim <hojin@amazon.com>
2026-03-21 12:16:47 +0000

    taygeta: Change from taygeta

--- vimrcs/nvim-treesitter.lua
+++ vimrcs/nvim-treesitter.lua
@@ -1,4 +1,8 @@
+local parser_dir = vim.fn.stdpath('data') .. '/treesitter'
+vim.opt.runtimepath:append(parser_dir)
+
 require 'nvim-treesitter.configs'.setup {
+    parser_install_dir = parser_dir,
     ensure_installed = {
         -- languages
         "awk", "bash", "c", "cmake", "cpp", "css", "csv", "diff", "dockerfile",
