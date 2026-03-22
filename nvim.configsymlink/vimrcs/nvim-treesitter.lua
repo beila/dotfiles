d3d647c4edb1483dce7eff7bb71c7ebeb5a36c02
Hojin Ghim <beila@kaist.ac.kr>
2026-03-22 23:08:06 +0000

    taygeta: feat: add vim-sleuth, which-key, and treesitter textobjects config

--- nvim.configsymlink/vimrcs/nvim-treesitter.lua
+++ nvim.configsymlink/vimrcs/nvim-treesitter.lua
@@ -21,4 +21,30 @@ require 'nvim-treesitter.configs'.setup {
     auto_install = true,
     highlight = { enable = true, },
     indent = { enable = true, },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+            },
+        },
+        move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+                [']f'] = '@function.outer',
+                [']a'] = '@parameter.outer',
+            },
+            goto_previous_start = {
+                ['[f'] = '@function.outer',
+                ['[a'] = '@parameter.outer',
+            },
+        },
+    },
 }
