c26db0e0302fe0a3c14daeddd2f3a02bc8753b17
Hojin Ghim <beila@kaist.ac.kr>
2026-03-24 10:56:51 +0000

    feat: add limelight-vim plugin with auto-enable for text and markdown files

--- nvim.configsymlink/vimrcs/my-markdown.lua
+++ nvim.configsymlink/vimrcs/my-markdown.lua
@@ -7,5 +7,5 @@ require('lspconfig').marksman.setup({})
 -- 테이블 편집이 편하도록 마크다운에서 자동 활성화 (<leader>tm 으로 토글)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
-    command = "TableModeEnable",
+    command = "silent! TableModeEnable",
 })
