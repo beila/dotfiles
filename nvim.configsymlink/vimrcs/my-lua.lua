96a707ae77bfbd3c498b565d8878c84966001b21
Hojin Ghim <beila@kaist.ac.kr>
2026-03-26 22:56:17 +0000

    fix: improve lua_ls config with runtime and workspace settings

--- nvim.configsymlink/vimrcs/my-lua.lua
+++ nvim.configsymlink/vimrcs/my-lua.lua
@@ -5,9 +5,11 @@
 require('lspconfig').lua_ls.setup({
     settings = {
         Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+            },
+        },
+    },
 })
