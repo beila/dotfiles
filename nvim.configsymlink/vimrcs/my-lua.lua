c8aab0bf268e4c2d8b7eb526dfcd39da9b49af01
Hojin Ghim <beila@kaist.ac.kr>
2026-03-26 23:04:01 +0000

    fix: simplify lua_ls config and skip it in mason default handler

--- nvim.configsymlink/vimrcs/my-lua.lua
+++ nvim.configsymlink/vimrcs/my-lua.lua
@@ -2,25 +2,14 @@
 -- Tools installed via nix in nvim.nix: lua-language-server, selene, stylua
 
 require('lspconfig').lua_ls.setup({
-    on_init = function(client)
-        if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-                return
-            end
-        end
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+    settings = {
+        Lua = {
             runtime = { version = 'LuaJIT' },
             diagnostics = { globals = { 'vim' } },
             workspace = {
                 checkThirdParty = false,
                 library = vim.api.nvim_get_runtime_file('', true),
             },
-        })
-    end,
-    settings = {
-        Lua = {
-            diagnostics = { globals = { 'vim' } },
         },
     },
 })
