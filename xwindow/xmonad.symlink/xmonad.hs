fa5c921342f7dea7d07ee286240a629ecb6245fd
Hojin Ghim <beila@kaist.ac.kr>
2026-04-02 12:36:14 +0100

    refactor: reorganize xmonad.hs with sorted imports and section headers

--- xwindow/xmonad.symlink/xmonad.hs
+++ xwindow/xmonad.symlink/xmonad.hs
@@ -2,26 +2,61 @@ import Control.Monad
 import Data.Maybe
 import Data.Monoid (All(..))
 import System.Directory (getHomeDirectory, setCurrentDirectory)
-import qualified Data.List as L (find,filter)
+import qualified Data.List as L (filter, find)
 import qualified Data.Map as M (member)
 import qualified XMonad.StackSet as W
+
 import XMonad
+import XMonad.Actions.CopyWindow
 import XMonad.Actions.CycleWS
 import XMonad.Config.Gnome
-import XMonad.Util.NamedScratchpad
-import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Actions.CopyWindow
 import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
 import XMonad.Hooks.Rescreen
+import XMonad.Hooks.SetWMName
 import XMonad.Layout.PerWorkspace
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.NamedScratchpad
 
--- Scratchpads: two independent floating ghostty terminals
--- End key toggles scratchpad1, PgDn toggles scratchpad2
+import Graphics.X11.ExtraTypes.XF86
+
+------------------------------------------------------------------------
+-- Main
+------------------------------------------------------------------------
+
+main = xmonad $ ewmhFullscreen $ rescreenHook rescreenCfg myConfig
+
+myConfig = gnomeConfig
+    { terminal = "gnome-terminal"
+    , startupHook = composeAll [
+        io (getHomeDirectory >>= setCurrentDirectory),
+        -- https://bbs.archlinux.org/viewtopic.php?pid=744577#p744577
+        setWMName "LG3D",
+                  -- https://github.com/texttheater/xminid/blob/master/xmonad.hs
+        startupHook gnomeConfig,
+        fullscreenStartupHook,
+        spawn "pgrep xfce4-panel || xfce4-panel",
+        spawn "pgrep albert || albert"
+    ]
+    , handleEventHook = handleEventHook gnomeConfig <> rescueOffscreenHook
+    , modMask = mod4Mask
+    -- https://wiki.haskell.org/Xmonad/General_xmonad.hs_config_tips#ManageHook_examples
+    , workspaces = myWorkspaces
+    -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration#Final_Touches
+    -- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Make_space_for_a_panel_dock_or_tray
+    , manageHook = myManageHook
+    , layoutHook = avoidStruts $ layoutHook gnomeConfig
+    } `additionalKeys` myKeys
+
+myWorkspaces = ["1:browser", "2:mail", "3:nvim", "4", "5", "6", "7:calendar", "8:meeting", "9:messenger"]
+
+------------------------------------------------------------------------
+-- Scratchpads
+------------------------------------------------------------------------
+
+-- Two independent floating ghostty terminals
 -- Positioning handled by adaptiveFloat based on screen orientation
 myScratchpads = [ NS "ghostty1" "ghostty --x11-instance-name=scratchpad1 --working-directory=$HOME -e $HOME/.dotfiles/bin/zellij-cycle scratch1"
                      (appName =? "scratchpad1")
@@ -89,6 +124,10 @@ refloatAdaptive isFirst w = do
     sc <- withWindowSet $ return . screenRect . W.screenDetail . W.current
     windows $ W.float w (scratchpadRect isFirst sc)
 
+------------------------------------------------------------------------
+-- Window rules
+------------------------------------------------------------------------
+
 -- Copy the managed window (not the focused one) to all workspaces
 copyToAllHook :: ManageHook
 copyToAllHook = ask >>= \w -> doF (\s -> foldr (copyWindow w . W.tag) s (W.workspaces s))
@@ -138,7 +177,9 @@ myManageHook = composeAll
     , namedScratchpadManageHook myScratchpads
     ]
 
-main = xmonad $ ewmhFullscreen $ rescreenHook rescreenCfg myConfig
+------------------------------------------------------------------------
+-- Monitor hotplug
+------------------------------------------------------------------------
 
 -- After monitor hotplug, swap NSP off any visible screen
 rescreenCfg = def { afterRescreenHook = fixNSP }
@@ -149,29 +190,9 @@ fixNSP = withWindowSet $ \ws -> do
             (w:_) -> windows $ W.greedyView (W.tag w)
             []    -> return ()
 
-myConfig = gnomeConfig
-    { terminal = "gnome-terminal"
-    , startupHook = composeAll [
-        io (getHomeDirectory >>= setCurrentDirectory),
-        -- https://bbs.archlinux.org/viewtopic.php?pid=744577#p744577
-        setWMName "LG3D",
-                  -- https://github.com/texttheater/xminid/blob/master/xmonad.hs
-        startupHook gnomeConfig,
-        fullscreenStartupHook,
-        spawn "pgrep xfce4-panel || xfce4-panel",
-        spawn "pgrep albert || albert"
-    ]
-    , handleEventHook = handleEventHook gnomeConfig <> rescueOffscreenHook
-    , modMask = mod4Mask
-    -- https://wiki.haskell.org/Xmonad/General_xmonad.hs_config_tips#ManageHook_examples
-    , workspaces = myWorkspaces
-    -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration#Final_Touches
-    -- https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Make_space_for_a_panel_dock_or_tray
-    , manageHook = myManageHook
-    , layoutHook = avoidStruts $ layoutHook gnomeConfig
-    } `additionalKeys` myKeys
-
-myWorkspaces = ["1:browser", "2:mail", "3:nvim", "4", "5", "6", "7:calendar", "8:meeting", "9:messenger"]
+------------------------------------------------------------------------
+-- Key bindings
+------------------------------------------------------------------------
 
 -- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen%27s_Configuration#Customizing_xmonad
 myKeys = [ ((mod4Mask .|. mod1Mask, xK_l), spawn "gnome-screensaver-command --lock")
@@ -196,7 +217,11 @@ myKeys = [ ((mod4Mask .|. mod1Mask, xK_l), spawn "gnome-screensaver-command --lo
     , (f, m) <- [(W.view, 0), (W.shift, shiftMask), (W.greedyView, controlMask), (myGreedyView, mod2Mask)]
     ]
 
--- TODO let's make it lruView
+------------------------------------------------------------------------
+-- Workspace switching
+------------------------------------------------------------------------
+
+-- TODO: make this lruView
 -- Copied from https://hackage.haskell.org/package/xmonad-0.15/docs/src/XMonad.StackSet.html#greedyView
 myGreedyView :: (Eq s, Eq i) => i -> W.StackSet i l a s sd -> W.StackSet i l a s sd
 myGreedyView w ws
@@ -208,7 +233,10 @@ myGreedyView w ws
      | otherwise = ws
    where wTag = (w == ) . W.tag
 
--- Rescue windows that move themselves offscreen (e.g. Zoom bug)
+------------------------------------------------------------------------
+-- Rescue offscreen windows (e.g. Zoom moving itself to x=12984)
+------------------------------------------------------------------------
+
 rescueOffscreenHook :: Event -> X All
 rescueOffscreenHook ConfigureEvent{ev_window = w, ev_x = ex, ev_y = ey, ev_width = ew, ev_height = eh} = do
     when (ew > 100 && eh > 100) $ do  -- ignore tiny windows (trays, etc.)
@@ -224,6 +252,10 @@ rescueOffscreenHook ConfigureEvent{ev_window = w, ev_x = ex, ev_y = ey, ev_width
                     windows $ W.float w (W.RationalRect 0.1 0.1 0.5 0.5)
     return (All True)
 rescueOffscreenHook _ = return (All True)
+
+------------------------------------------------------------------------
+-- EWMH fullscreen support
+------------------------------------------------------------------------
 
 -- Advertise fullscreen support to EWMH
 fullscreenStartupHook :: X ()
