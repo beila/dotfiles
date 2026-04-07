5a9573a9161cdd458c8ca5b0d64a6e826a0413dd
Hojin Ghim <hojin@amazon.com>
2026-04-07 16:42:37 +0100

    refactor: migrate html/nim/vim/xml LSPs to native vim.lsp.config API

--- nvim.configsymlink/vimrcs/my-nim.lua
+++ nvim.configsymlink/vimrcs/my-nim.lua
@@ -2,4 +2,5 @@
 -- Tools installed via nix in nvim.nix: nimlangserver
 -- Formatter: nimpretty (bundled with nim, install nim in home.nix if needed)
 
-require('lspconfig').nim_langserver.setup({})
+vim.lsp.config.nim_langserver = {}
+vim.lsp.enable('nim_langserver')
