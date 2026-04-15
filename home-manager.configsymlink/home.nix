8999baccfee07f6841e5eae1a021919857e5de29
Hojin Ghim <beila@kaist.ac.kr>
2026-04-15 15:20:01 +0000

    feat: add ghostty terminfo to home configuration

--- home-manager.configsymlink/home.nix
+++ home-manager.configsymlink/home.nix
@@ -29,6 +29,7 @@
         pkgs.fzf
         pkgs.git
         (config.lib.nixGL.wrap pkgs.ghostty)
+        pkgs.ghostty.terminfo
         pkgs.hishtory
         pkgs.jetbrains-mono # For OSD popups
         pkgs.nerd-fonts.jetbrains-mono  # For OSD popups
@@ -80,16 +81,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".terminfo".source = "${pkgs.ghostty.terminfo}/share/terminfo";
   };
 
   # Home Manager can also manage your environment variables through
@@ -109,7 +101,6 @@
   #  /etc/profiles/per-user/hojin/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
