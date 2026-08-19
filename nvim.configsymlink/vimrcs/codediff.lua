b628a98fc09cdc45f93b739b142ad9d056fd16a3
Hojin Ghim <beila@kaist.ac.kr>
2026-08-19 12:26:34 +0100

    docs(nvim): note CodeDiff return-to-origin tab behavior

--- nvim.configsymlink/vimrcs/codediff.lua
+++ nvim.configsymlink/vimrcs/codediff.lua
@@ -7,3 +7,37 @@ require("codediff").setup({
 		},
 	},
 })
+
+local origins = {}
+local group = vim.api.nvim_create_augroup("CodeDiffReturnToOrigin", { clear = true })
+
+vim.api.nvim_create_autocmd("User", {
+	group = group,
+	pattern = "CodeDiffOpen",
+	callback = function(args)
+		local tabpage = args.data and args.data.tabpage
+		local origin = vim.api.nvim_list_tabpages()[vim.fn.tabpagenr("#")]
+		if tabpage and origin and origin ~= tabpage then
+			origins[tabpage] = origin
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	group = group,
+	pattern = "CodeDiffClose",
+	callback = function(args)
+		local tabpage = args.data and args.data.tabpage
+		local origin = tabpage and origins[tabpage]
+		if not origin then
+			return
+		end
+		origins[tabpage] = nil
+
+		vim.schedule(function()
+			if not vim.api.nvim_tabpage_is_valid(tabpage) and vim.api.nvim_tabpage_is_valid(origin) then
+				vim.api.nvim_set_current_tabpage(origin)
+			end
+		end)
+	end,
+})
